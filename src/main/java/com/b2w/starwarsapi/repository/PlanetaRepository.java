123587336742a48bb37661e012cd528563f898a8
unknown <lscunha@brasilseg.com.br>
2021-05-22 16:03:37 -0300

    Operações básicas do repositório

--- src/main/java/com/b2w/starwarsapi/repository/PlanetaRepository.java
+++ src/main/java/com/b2w/starwarsapi/repository/PlanetaRepository.java
@@ -1,4 +1,13 @@
 package com.b2w.starwarsapi.repository;
 
+import com.b2w.starwarsapi.domain.Planeta;
+
+import java.util.List;
+
 public interface PlanetaRepository {
+    Planeta salvar(Planeta planeta);
+    List<Planeta> listarTodos();
+    Planeta buscarPorId(String planetaId);
+    void deletar(String planetaId);
+
 }
--- src/main/java/com/b2w/starwarsapi/repository/impl/PlanetaRepositoryImpl.java
+++ src/main/java/com/b2w/starwarsapi/repository/impl/PlanetaRepositoryImpl.java
@@ -1,8 +1,45 @@
 package com.b2w.starwarsapi.repository.impl;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.b2w.starwarsapi.domain.Planeta;
 import com.b2w.starwarsapi.repository.PlanetaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class PlanetaRepositoryImpl implements PlanetaRepository {
+
+    @Autowired
+    DynamoDBMapper dynamoDBMapper;
+
+    @Override
+    public Planeta salvar(Planeta planeta) {
+        dynamoDBMapper.save(planeta);
+        return planeta;
+    }
+
+    @Override
+    public List<Planeta> listarTodos() {
+        return dynamoDBMapper.query(Planeta.class, new DynamoDBQueryExpression<Planeta>());
+    }
+
+    @Override
+    public Planeta buscarPorId(String planetaId) {
+        return dynamoDBMapper.load(Planeta.class, planetaId);
+    }
+
+    @Override
+    public void deletar(String planetaId) {
+        Planeta planeta = dynamoDBMapper.load(Planeta.class, planetaId);
+        dynamoDBMapper.delete(planeta);
+    }
 }
